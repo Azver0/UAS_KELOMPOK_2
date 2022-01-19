d825d7e6f928f18f8e76b991e10b0b86ebc539b6
Azver0 <96857502+Azver0@users.noreply.github.com>
2022-01-20 02:39:14 +0700

    Update MainMenu

--- UAS/src/com/itenas/oop/uas/menu/MainMenu.java
+++ UAS/src/com/itenas/oop/uas/menu/MainMenu.java
@@ -6,21 +6,18 @@ import com.itenas.oop.uas.bean.User;
 
 import com.itenas.oop.uas.dao.UserDAO;
 import com.itenas.oop.uas.daoimpl.UserDaoImpl;
+import com.itenas.oop.uas.view.Form;
+import com.itenas.oop.uas.view.TableView;
 
-import com.itenas.oop.uas.bean.Customer;
-import com.itenas.oop.uas.view.StructPrint;
-
-public class MainMenu {
-
+public class MainMenu{
 	public static void main(String[] args) {
 		mainMenu();
 	}
-	
+
 	public static void mainMenu() {
 		int gameChoice;
-		
+	
 		Scanner scan = new Scanner(System.in);
-		Customer cust = new Customer();	
 		UserDAO operation = new UserDaoImpl();
 		User user;
 		String email, password;
@@ -39,6 +36,8 @@ public class MainMenu {
 			user = operation.getUserByEmailAndPassword(email, password);
 			if (user != null) {
 				login = true;
+				
+				// Menu Customer
 				System.out.println();
 				System.out.println("========================");
 				System.out.println("          Menu          ");
@@ -52,17 +51,56 @@ public class MainMenu {
 				System.out.println("------------------------");
 				System.out.print("Pilihan Anda : ");				gameChoice = scan.nextInt();
 				
-				while (!(gameChoice == 1 || gameChoice == 2 || gameChoice == 3)) {
+				while (!(gameChoice == 1 || gameChoice == 2 || gameChoice == 3 || gameChoice == 4)) {
 		        	System.out.println("Warning: Menu yang Anda masukkan salah!");
 		        	System.out.println();
 		        	System.out.print("Silahkan pilih menu kembali: ");
 		        	gameChoice = scan.nextInt();
 		        }
 				
-				TopUp tm = new TopUp(gameChoice, cust);
+				TopUp tm = new TopUp(gameChoice, user.getEmail());
 				
 				scan.close();
 				
+				// Menu Admin
+				
+				/*
+				 	System.out.println();
+					System.out.println("========================");
+					System.out.println("          Menu          ");
+					System.out.println("========================");
+					System.out.println("1. Masukan Data ");
+					System.out.println("2. Lihat Data ");
+					System.out.println("3. Update Data ");
+					System.out.println("4. Hapus Data");
+					System.out.println();
+					System.out.println("0. Log out");
+					System.out.println("------------------------");
+					System.out.print("Pilihan Anda : ");				menuChoice = scan.nextInt();
+					while (!(menuChoice == 1 || menuChoice == 2 || menuChoice == 0)) {
+			        	System.out.println("Warning: Menu yang Anda masukkan salah!");
+			        	System.out.println();
+			        	System.out.print("Silahkan pilih menu kembali: ");
+			        	menuChoice = scan.nextInt();
+			        }
+					switch (menuChoice) {
+					case 1 :
+						Form.registerForm();
+					break;
+					
+					case 2 :
+						TableView.execute();
+					break;
+					
+					case 3 : 
+						
+					break;
+					
+					case 4 :
+						Form.deleteForm();
+					break;
+					}
+				 */
 
 			} else {
 				System.out.println("Email atau password yang Anda masukkan salah, coba lagi...!");
