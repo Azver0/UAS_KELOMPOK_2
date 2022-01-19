39e3a8982e631061cd6776452f1de00007fbf00f
Azver0 <96857502+Azver0@users.noreply.github.com>
2022-01-20 02:37:14 +0700

    Update UserDaoImpl

--- UAS/src/com/itenas/oop/uas/daoimpl/UserDaoImpl.java
+++ UAS/src/com/itenas/oop/uas/daoimpl/UserDaoImpl.java
@@ -4,9 +4,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import com.itenas.oop.uas.bean.User;
 import com.itenas.oop.uas.dao.UserDAO;
 import com.itenas.oop.uas.utils.DatabaseUtil;
+import com.mysql.cj.protocol.Resultset;
 
 public class UserDaoImpl implements UserDAO {
 
@@ -89,5 +92,69 @@ public class UserDaoImpl implements UserDAO {
 		
 		return user.getFirstName();
 	}
-
+	
+	@Override
+	public void register(User user) {
+		DatabaseUtil db = new DatabaseUtil();
+		String query;
+		try {
+			db.connect();
+			query = "INSERT INTO User VALUES ("
+					+ "'" + user.getEmail() + "',"
+					+ "'" + user.getPassword() + "',"
+					+ "'" + user.getFirstName() + "');";
+			
+			db.executeQuery(query);
+			System.out.println("Data berhasil ditambahkan");
+			
+		} catch (Exception ex) {
+			System.out.println("Terjadi error : " + ex.getMessage());
+		}
+	}
+	
+	public void deleteUser(User user) {
+		DatabaseUtil db = new DatabaseUtil();
+		String query;
+		try {
+			db.connect();
+			query = "DELETE FROM User WHERE Email = '" + user.getEmail() + "';";
+			db.executeQuery(query);
+			System.out.println("Data berhasil dihapus...");
+		} catch (Exception ex) {
+			System.out.println("Terjadi Error : " + ex.getMessage());
+		}
+	}
+	
+	public List<User> getAllUser() {
+		List<User> listUser = new ArrayList<User>();
+		DatabaseUtil db = new DatabaseUtil();
+		
+		try {
+			db.connect();
+			
+			String query = "SELECT * FROM User";
+			
+			ResultSet rs = db.readData(query);
+			
+			while (rs.next()) {
+				User user = new User();
+				
+				user.setEmail(rs.getObject(1).toString());
+				user.setPassword(rs.getObject(2).toString());
+				user.setFirstName(rs.getObject(3).toString());
+				
+				listUser.add(user);
+				
+			}
+			
+			db.disconnect();
+			
+		} catch (Exception ex) {
+			System.out.println("Terjadi Error : " + ex.getMessage());
+		}
+		
+		return listUser;
+	}
+	
+	
 }
