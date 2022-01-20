6dc59dba96d93f884e911ab0e6a9c5cf93d46d6b
Azver0 <96857502+Azver0@users.noreply.github.com>
2022-01-20 12:35:20 +0700

    Revisi

--- UAS/src/com/itenas/oop/uas/dao/TransaksiDAO.java
+++ UAS/src/com/itenas/oop/uas/dao/TransaksiDAO.java
@@ -1,8 +1,10 @@
 package com.itenas.oop.uas.dao;
 
+import java.util.List;
+
 import com.itenas.oop.uas.bean.Transaksi;
-import com.itenas.oop.uas.bean.User;
 
 public interface TransaksiDAO {
 	public void setTransaksi(String username, Transaksi transaksi);
+	public List<Transaksi> getAllTransaksi();
 }
--- UAS/src/com/itenas/oop/uas/dao/UserDAO.java
+++ UAS/src/com/itenas/oop/uas/dao/UserDAO.java
@@ -6,10 +6,14 @@ import com.itenas.oop.uas.bean.User;
 
 public interface UserDAO {
 	public User getUserByEmailAndPassword(String email, String password);
+	public User getUserByEmailAndPasswordAdmin(String email, String password);
 	public String getUserForStruct(String email);
 	
 	public List<User> getAllUser();
 	public void register(User user);
 	public void deleteUser(User user);
+	public void updateUser(User admin);
+	public void registerAdmin(User user);
+	
 }
 
